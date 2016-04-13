dde9826b225d20ffa27e03dd9facf1f3a31dcc62
John-Lihach <jal60237@marymount.edu>
2016-04-13 08:50:52 -0400

    Update javascript.js
    Visual and screen-size compatibility update.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 var clicked = 1;
-function enlarge(x,y)
+function enlarge(x,y,z)
 {
 	if (clicked == 1) {
 		document.getElementById(y).style.width = "70%";
@@ -7,25 +7,52 @@ function enlarge(x,y)
 		document.getElementById(y).style.zIndex = 5;
 		document.getElementById(y).style.position = "fixed";
 		document.getElementById(y).style.display = "block";
-		document.getElementById(y).style.boxShadow = "10px 10px 25px #000000";
+		if (z=="video"){
+			document.getElementById(y).style.boxShadow = "10px 10px 25px #000000";
+			document.getElementById(x).style.width = "100%";
+			document.getElementById(x).style.height = "100%";
+		}
+		else {
+			if (document.getElementById(x).naturalWidth / window.innerWidth > document.getElementById(x).naturalHeight / window.innerHeight){
+				document.getElementById(x).style.width = "100%";
+				var ratio = document.getElementById(x).naturalWidth / window.innerWidth
+				var ht = document.getElementById(x).naturalHeight / ratio * .7
+				document.getElementById(x).style.height = ht;
+				document.getElementById(x).style.position = "absolute";
+				document.getElementById(x).style.left = "0";
+				document.getElementById(x).style.right = "0";
+				document.getElementById(x).style.marginLeft = "auto";
+				document.getElementById(x).style.marginRight = "auto";
+				document.getElementById(x).style.top = "50%";
+				document.getElementById(x).style.marginTop = -1*ht*.5;
+			}
+		}
 		document.getElementById(y).style.top = 85
-		document.getElementById(x).style.width = "100%";
-		document.getElementById(x).style.height = "100%";
-		document.getElementById("description").style.display = "block"
+		document.getElementById("description").style.display = "block";
+		document.getElementById("dim_background").style.display = "block";
 		clicked = 2;
-		add_comments(x);
+		add_comments(x,z);
 	}
 	else {
-		document.getElementById(x).style.width = 500;
-		document.getElementById(x).style.height = 332;
-		document.getElementById(y).style.width = 508;
-		document.getElementById(y).style.height = 340;
+		document.getElementById(y).style.width = 504;
+		document.getElementById(y).style.height = 336;
 		document.getElementById(y).style.zIndex = 0;
-		document.getElementById(y).style.boxShadow = "none";
+		if (z=="video"){
+			document.getElementById(y).style.boxShadow = "none";
+			document.getElementById(x).style.width = 500;
+			document.getElementById(x).style.height = 332;
+		}
+		else{
+			document.getElementById(x).style.height = "100%";
+			document.getElementById(x).style.width = "auto";
+			document.getElementById(x).style.position = "static";
+			document.getElementById(x).style.marginTop = 0;
+		}
 		document.getElementById(y).style.position = "relative";
 		document.getElementById(y).style.display = "inline-block";
-		document.getElementById(y).style.top = 0
-		document.getElementById("description").style.display = "none"
+		document.getElementById(y).style.top = 0;
+		document.getElementById("description").style.display = "none";
+		document.getElementById("dim_background").style.display = "none";
 		clicked = 1;
 	}
 }
@@ -35,22 +62,22 @@ var comments = ["So cool",
 "Awesome!",
 "Lammmmeeee",
 "Needs more cowbell",
-"I really love the style of this video",
+"I really love the style of this",
 "OMG, this is the most amazing thing ever",
 "lol",
 "Check out my youtube channel",
 "So much spam in this comment section",
 "Another one",
-"I wish this video was longer",
+"I wish there was more!",
 "This is dumb",
 "Love it!",
-"Great video, can't wait for more",
+"Really great, can't wait for more",
 "Haha, beautiful",
 "Psh."];
 
 var comment_table = ["tr1","tr2","tr3","tr4","tr5","tr6","tr7","tr8"]
 
-var title_array = ["X-men:Apocalypse | Official Trailer[HD] | 20th Century FOX",
+var video_title_array = ["X-men:Apocalypse | Official Trailer[HD] | 20th Century FOX",
 "ZAYN - BeFoUr (Audio)",
 "Little Boy Befriends a Baby Deer",
 "Full Speech: Donald Trump Rally in Tucson, AZ (3-19-16)",
@@ -63,9 +90,27 @@ var title_array = ["X-men:Apocalypse | Official Trailer[HD] | 20th Century FOX",
 "GHOSTBUSTERS - Official Trailer (HD)",
 "Jake Gyllenhaal Bombed His Lord of the Rings Audition"];
 
-function add_comments(x)
+var photo_title_array = ["Blurry Cityscape",
+"Streetlight",
+"Moths on Streetlight",
+"Telephone Pole",
+"Air and Space Museum - DC",
+"Smithsonian Castle",
+"Sloth",
+"Duck",
+"Wolves",
+"Corgi",
+"Huskeys",
+"Lion cub"];
+
+function add_comments(x,z)
 {
-	document.getElementById("title").innerHTML = title_array[x-1];
+	if (z == "video"){
+		document.getElementById("title").innerHTML = video_title_array[x-1];
+	}
+	else {
+		document.getElementById("title").innerHTML = photo_title_array[x-1];
+	}
 	var comments_used = [];
 	for (i=0; i < 8; i++){
 		do {var number = Math.floor((Math.random()*16)+1);
@@ -75,4 +120,16 @@ function add_comments(x)
 		document.getElementById(comment_table[i]).innerHTML = comments[number-1];
 	}
 	var comments_used = [];
+}
+
+
+function purchase_a()
+{
+	document.getElementById("purchase_wrapper").style.display = "block";
+	setTimeout(purchase_b, 1000);
+}
+
+function purchase_b()
+{
+	document.getElementById("purchase_wrapper").style.display = "none";
 }
